9f0db95385d7b026bfd4ab7c745c2674d624a41c
Martin Szigeti <38685413+Skerre@users.noreply.github.com>
2023-06-06 17:01:16 +0300

    Qgis Processing works now

---
+++ QGIS script/Getenvinformation.py
@@ -0,0 +1,13 @@
+#import sys
+#import pandas as pd
+#paths = sys.path
+#df = pd.DataFrame({'paths':paths})
+#df.to_csv(r'C:\Users\martin\PycharmProjects\HeatMapMaker/qgis_sys_paths.csv', index=False)
+
+import os
+import json
+env = dict(os.environ)
+rem = ['SECURITYSESSIONID', 'LaunchInstanceID', 'TMPDIR']
+_ = [env.pop(r, None) for r in rem]
+with open(r'C:\Users\martin\PycharmProjects\HeatMapMaker/qgis_env.json', 'w') as f:
+    json.dump(env, f, ensure_ascii=False, indent=4)
--- main.py
+++ main.py
@@ -1,15 +1,47 @@
 """ QGIS TESTING"""
 
 import requests
-import sys
 from zipfile import ZipFile
-import os
 import glob
 from qgis.core import *
-from qgis import processing
+import os
+import sys
+import json
+import pandas as pd
 
-# sys.path.append("C:/Program Files/QGIS 3.30.0/apps/qgis/python/plugins")
-# from plugins import processing
+# set up system paths
+qspath = r'C:\Users\martin\PycharmProjects\HeatMapMaker\QGIS script/qgis_sys_paths.csv'
+# provide the path where you saved this file.
+paths = pd.read_csv(qspath).paths.tolist()
+sys.path += paths
+# set up environment variables
+qepath = r'C:\Users\martin\PycharmProjects\HeatMapMaker\QGIS script/qgis_env.json'
+js = json.loads(open(qepath, 'r').read())
+for k, v in js.items():
+    os.environ[k] = v
+# In special cases, we might also need to map the PROJ_LIB to handle the projections
+# for mac OS
+# .environ['PROJ_LIB'] = '/Applications/Qgis.app/Contents/Resources/proj'
+
+# qgis library imports
+import PyQt5.QtCore
+import qgis.PyQt.QtCore
+from qgis.core import (QgsApplication,
+                       QgsProcessingFeedback,
+                       QgsProcessingRegistry)
+from qgis.analysis import QgsNativeAlgorithms
+
+feedback = QgsProcessingFeedback()
+# initializing processing module
+QgsApplication.setPrefixPath(js['HOME'], True)
+qgs = QgsApplication([], False)
+qgs.initQgis() # use qgs.exitQgis() to exit the processing module at the end of the script.
+# initialize processing algorithms
+from processing.core.Processing import Processing
+Processing.initialize()
+import processing
+QgsApplication.processingRegistry().addProvider(QgsNativeAlgorithms())
+
 
 def download():
     inputcountry = input("Country: ")
@@ -33,13 +65,19 @@ def process(data, ctr):
     print("Entering Processing")
     filtered_list = [item for item in data if item.endswith('2.shp') or item.endswith('0.shp')]
     layer = QgsVectorLayer("/{}".format(ctr), filtered_list[0], "ogr")
-    processing.run("qgis:randompointsinsidepolygons",
-                           {'INPUT': layer,
+    processing.run("qgis:randompointsinsidepolygons", {'INPUT': layer,
                     'STRATEGY': 0, 'VALUE': 100,
-                    'MIN_DISTANCE': None, 'OUTPUT': '/{}/randompoints_temp.shp'.format(ctr)})
-    processing.run("qgis:heatmapkerneldensityestimation",
-                           {'INPUT': layer, 'RADIUS': 0.5, 'RADIUS_FIELD': '', 'PIXEL_SIZE': 0.005, 'WEIGHT_FIELD': '',
+                    'MIN_DISTANCE': None, 'OUTPUT': './{}/randompoints_temp.shp'.format(ctr)})
+    processing.run("qgis:heatmapkerneldensityestimation", {'INPUT': layer, 'RADIUS': 0.5, 'RADIUS_FIELD': '', 'PIXEL_SIZE': 0.005, 'WEIGHT_FIELD': '',
                     'KERNEL': 0, 'DECAY': 0, 'OUTPUT_VALUE': 0, 'OUTPUT': 'TEMPORARY_OUTPUT'})
+
+    processing.run("gdal:cliprasterbymasklayer", {
+        'INPUT': 'C:/Users/martin/Documents/QGIS_temp/processing_yRndbJ/50c7494b1a7a456c8a388683633495bb/OUTPUT.tif',
+        'MASK': 'C:/Users/martin/PycharmProjects/HeatMapMaker/ZMB/ZMB_adm0.shp', 'SOURCE_CRS': None, 'TARGET_CRS': None,
+        'TARGET_EXTENT': None, 'NODATA': -1, 'ALPHA_BAND': False, 'CROP_TO_CUTLINE': True, 'KEEP_RESOLUTION': False,
+        'SET_RESOLUTION': False, 'X_RESOLUTION': None, 'Y_RESOLUTION': None, 'MULTITHREADING': False, 'OPTIONS': '',
+        'DATA_TYPE': 0, 'EXTRA': '', 'OUTPUT': 'TEMPORARY_OUTPUT'})
+
     print("End of processing")
 
 
